4edb794b63777fc2d2b428533e5bf843c11a0c1f
cclin <181250083@smail.nju.edu.cn>
2019-07-08 07:16:36 +0800

    'tripack'

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,18 @@
     <el-container>
       <el-header :style="{height: 'none', padding: 0}">
         <el-dropdown :style="{float: 'left', color: 'white'}" class="setting">
-          <span class="header-setting">
+          <div class="header-setting">
             設置<i class="el-icon-setting el-icon--right"></i>
-          </span>
+          </div>
           <el-dropdown-menu slot="dropdown">
 
           </el-dropdown-menu>
         </el-dropdown>
-        <span class="title">TRIPACK</span>
+        <div class="title">TRIPACK</div>
         <el-dropdown :style="{float: 'right', color: 'white'}" class="setting">
-          <span class="header-setting">
+          <div class="header-setting">
             詳情<i class="el-icon-menu"></i>
-          </span>
+          </div>
           <el-dropdown-menu slot="dropdown">
 
           </el-dropdown-menu>
@@ -27,11 +27,20 @@
         </div>
         <div class="recommend">
           <span class="recommend-text">熱搜推薦:</span>
-          <el-button :style="{padding: '5px 10px', float: 'left'}" type="primary" round>環島</el-button>
+          <el-button :style="{padding: '5px 10px', float: 'left', margin: '5px 0 0 5px'}" type="primary" round>環島</el-button>
+          <el-button :style="{padding: '5px 10px', float: 'left', margin: '5px 0 0 5px'}" type="primary" round>台北</el-button>
         </div>
-
+        <div class="ads">廣告</div>
+        <div class="main-main"></div>
+        <div class="main-main"></div>
       </el-main>
-      <el-footer :style="{height: 'none', padding: 0}"></el-footer>
+
+      <el-footer :style="{height: 'none', padding: 0}">
+        <el-button :style="{padding: '5px 10px', float: 'left', margin: '10px 0 0 5px'}" type="primary" icon="el-icon-user-solid" size="small">客服</el-button>
+        <el-button :style="{padding: '5px 10px', float: 'right', margin: '11px 5px 0 0'}" type="primary" size="mini">新建旅行</el-button>
+        <el-button :style="{padding: '5px 10px', float: 'right', margin: '11px 5px 0 0'}" type="primary" size="mini">旅行日記</el-button>
+        <el-button :style="{padding: '5px 10px', float: 'right', margin: '11px 5px 0 0'}" type="primary" size="mini">我的</el-button>
+      </el-footer>
     </el-container>
   </div>
 </template>
@@ -75,12 +84,12 @@ html,body,#app {
 }
 
 .setting {
-  text-align: center;
   height: 100%;
 }
 
 .header-setting {
   font-size: 15px;
+  margin-top: 12px;
 }
 
 .el-icon-setting {
@@ -93,7 +102,8 @@ html,body,#app {
   float: left;
   display: block;
   text-align: center;
-  margin-left: 51px;
+  margin-top: 5px;
+  margin-left: 50px;
 }
 
 .el-main {
@@ -112,6 +122,19 @@ html,body,#app {
   margin-left: 5px;
   margin-top: 10px;
   color: white;
+}
+
+.ads {
+  height: 90px;
+  border-bottom-style: solid;
+  border-bottom-color: white;
+}
+
+.main-main {
+  height: 86px;
+  border-style: solid;
+  border-color: white;
+  margin: 5px 5px;
 }
 
 .el-footer {
